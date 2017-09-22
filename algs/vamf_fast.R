fce965dd6c625ba694a27a55e10d8d5c5d2d494a
Will Townes <will.townes@gmail.com>
2017-09-22 11:06:45 -0400

    more tinkering with fast vamf

--- algs/vamf_fast.R
+++ algs/vamf_fast.R
@@ -13,23 +13,30 @@ colNorms<-function(x){
 }
 
 Y<-as.matrix(Y_obs)
-L<-5
-nIter<-100
+L<-2
+nIter<-10
 eps<-.1 #gradient ascent step size
 s2v<-1; s2u<-.1; s2w<-10;
 N<-ncol(Y); G<-nrow(Y)
 s2y<-sd(Y)^2
 y0<-mean(Y)
-V<-matrix(rnorm(L*G,0,sv),nrow=L)
-V<-t(svd(V)$v)
-U<-matrix(rnorm(L*N,0,su),nrow=L)
+#V<-matrix(rnorm(L*G,0,sv),nrow=L)
+#V<-t(svd(V)$v)
+#U<-matrix(rnorm(L*N,0,su),nrow=L)
 w<-rowMeans(Y)-y0
 Z<-Y>0
 Zrs<-rowSums(Z)
 Yrs<-rowSums(Y)
 #rough estimate for dropout parameters a,b
-a<-.75
+a<-5
 pdet<-colMeans(Z) #detection rates for each cell
+detlm<-lm(t(Y)~pdet)
+Yrs<-t(resid(detlm))
+Ysvd<-svd(Yrs)
+V<-t(Ysvd$u)[1:L,]
+U<-Ysvd$d[1:L]*t(Ysvd$v)[1:L,]
+rownames(U)<-paste0("dim",1:L)
+plt_clst(t(U),ref)
 aec<-colMeans(Y) #average expression for each cell
 b<-aec + logit(pdet)/a #logit shift parameters for each cell's curve, a vector
 #boxplot(b~ref$batch)
@@ -45,7 +52,7 @@ for(i in 1:nIter){
     U[,n]<-U[,n]-eps*(crossprod(t(V)*Z[,n])/s2y+penalty)%*%U[,n]+eps*M[,n]
   }
   zUv<-vapply(1:G,function(g){Z[g,]%*%crossprod(U,V[,g])},FUN.VALUE=1.0)
-  y0<-y0-eps*(sum(Zrs)/s2y)*y0+eps*(a*sum(ZL)+(1/s2y)*(sum(Yrs)-sum(w%*%Z)-sum(zUv)))
+  #y0<-y0-eps*(sum(Zrs)/s2y)*y0+eps*(a*sum(ZL)+(1/s2y)*(sum(Yrs)-sum(w%*%Z)-sum(zUv)))
   w<-w-eps*(Zrs/s2y+1/s2w)*w+eps*(a*rowSums(ZL)+(Yrs-y0*Zrs-zUv)/s2y)
   M2<-U%*%(a*t(ZL)+(1/s2y)*t(Y-Z*(y0+w))) #LxG temporary
   penalty2<-(1/s2v)*diag(L)
@@ -65,6 +72,8 @@ for(i in 1:nIter){
   R<-y0+w+crossprod(V,U)
   s2y<-sd(Y-R*Z)^2
   Lambda<-t(t(R)>b)
+  #b<-b - eps*a*(colSums(Z)+colSums(Lambda))
+  #Lambda<-t(t(R)>b)
 }
 rownames(U)<-paste0("dim",1:L)
 plt_clst(t(U[1:2,]),ref)
